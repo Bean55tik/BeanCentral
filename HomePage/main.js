83592227a1ae716290a19c13f77266ccc05f2fcb
Bean55tik <156041762+Bean55tik@users.noreply.github.com>
2024-01-16 15:09:20 -0500

    Delete HomePage directory

--- HomePage/main.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".burger");
-const dropdown = document.querySelector(".dropdown");
-const fade = document.querySelector(".dropdown li a");
-
-hamburger.addEventListener("click", function () {
-  dropdown.classList.toggle("open");
-});
